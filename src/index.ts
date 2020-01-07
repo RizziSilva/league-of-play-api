9449f2e1ffb06098eb5d54d9206d887a7bf7e6b1
silva.william <52845549+RizziSilva@users.noreply.github.com>
2020-01-07 10:04:27 -0300

    Configuração basica do express.

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,19 @@
-import express from "express"
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
 
-const app = express()
-const port = 3000 || process.env.PORT
+const app = express();
+const port = 3000 || process.env.PORT;
 
-app.get("/", (req, res) => {
-    res.send("Starting Point!")
-})
+app.use(bodyParser.json({ limit: '1.1mb' }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('Starting Point!');
+});
 
 app.listen(port, () => {
     // tslint:disable-next-line:no-console
-    console.log(`server started at http://localhost:${port}`)
-})
+    console.log(`server started at http://localhost:${port}`);
+});
