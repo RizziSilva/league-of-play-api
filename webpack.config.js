bcfdd9f6743be432a193819c8493d49d4ae4b18e
silva.william <52845549+RizziSilva@users.noreply.github.com>
2020-01-08 15:25:11 -0300

    Start alias config and dynamic imports pollyfil

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    target: 'node',
+    resolve: {
+        extensions: ['.ts', '.js', '.json'],
+        alias: {
+            'api-config': path.resolve(__dirname, 'src/config/'),
+            'api-controller': path.resolve(__dirname, 'src/controller/'),
+            'api-middleware': path.resolve(__dirname, 'src/middleware/'),
+            'api-utils': path.resolve(__dirname, 'src/utils/'),
+        },
+    },
+    devtool: 'inline-source-map',
+    entry: {
+        app: ['./src/index.ts'],
+    },
+    output: {
+        path: path.resolve(__dirname, 'bundle'),
+        filename: 'bundle.js',
+        publicPath: 'bundle/',
+    },
+};
